77757da227cc22adf96adf4e7d97490324e6151d
Aloÿs Augustin <aloys.augustin@polytechnique.edu>
2015-07-18 11:45:36 +0200

    TLV struct

--- src/hncp_slicing.h
+++ src/hncp_slicing.h
@@ -7,6 +7,17 @@
 
 #include "hncp.h"
 
+struct hncp_slice_membership_data;
+typedef struct hncp_slice_membership_data hncp_slice_membership_data_s;
+typedef struct hncp_slice_membership_data *hncp_slice_membership_data_p;
+
+struct hncp_slice_membership_tlv {
+	uint32_t endpoint_id;
+	uint32_t slice_id;
+};
+
+
+
 int hncp_slicing_init(hncp hncp, const char *scriptpath);
 
 #endif /* HNCP_SLICING_H_ */
